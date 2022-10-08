f2317253df0b6791d752654706904438b3f100b3
ubermanu <manu@paca.happy-dev.fr>
2022-10-08 15:12:24 +0200

    remove observer causing the links to have unwanted params

--- view/frontend/web/js/hydrate.js
+++ view/frontend/web/js/hydrate.js
@@ -4,11 +4,6 @@ define([
 ], function ($, arrosoir) {
     'use strict';
 
-    const defaultOptions = {
-        history: false,
-        contentOnly: true,
-    }
-
     /**
      * Function to hydrate the #maincontent with the server side response.
      * @see https://github.com/ubermanu/arrosoir
@@ -17,31 +12,11 @@ define([
      * @param {object} options
      */
     return function (url, options = {}) {
-        options = $.extend({}, defaultOptions, options);
-
-        // Preserve the original URL, so we can use it in the history
-        const origUrl = url;
-
-        // Add the `isContent` GET param to the url
-        if (options.contentOnly) {
-            const params = arrosoir.params(url);
-            params.isContent = true;
-            url = params.apply(url);
-        }
-
         $('body').trigger('processStart');
 
-        // Force the history option to false and manually do it on callback
-        // Since we might have the `isContent` GET param, we don't want to push it to the history
-        const hydrateOptions = $.extend({}, options, { history: false });
-
-        arrosoir.hydrate('#maincontent', url, hydrateOptions).then(function () {
+        arrosoir.hydrate('#maincontent', url, options).then(function () {
             $('#maincontent').trigger('contentUpdated');
             $('body').trigger('processStop');
-
-            if (options.history) {
-                history.pushState({}, '', origUrl);
-            }
         });
     }
 });
